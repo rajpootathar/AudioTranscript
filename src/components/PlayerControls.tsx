e3f09f83bba6e472909f2a2aee4895aa2e1c6c50
rajpootathar <rajpootathar@gmail.com>
2025-01-28 19:05:43 +0500

    added swiper

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,37 +1,56 @@
 import React from 'react';
 import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import Slider from '@react-native-community/slider';
 
 const PlayerControls = () => {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity activeOpacity={0.8}>
-        <Image
-          source={require('../Assets/Images/backward.png')}
-          style={styles.icons}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.8} style={styles.playAndPauseButton}>
-        <Image
-          source={require('../Assets/Images/play.png')}
-          style={styles.playAndPauseIcon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.8}>
-        <Image
-          source={require('../Assets/Images/fast-forward.png')}
-          style={styles.icons}
-        />
-      </TouchableOpacity>
-    </View>
+    <>
+      <Slider
+        style={styles.slider}
+        value={2}
+        onValueChange={value => {
+          console.log(value);
+        }}
+        minimumValue={0}
+        maximumValue={20}
+        minimumTrackTintColor="#99BD01"
+        maximumTrackTintColor="#DAE4ED"
+        onSlidingComplete={value => {
+          console.log(value);
+        }}
+      />
+      <View style={styles.container}>
+        <TouchableOpacity activeOpacity={0.8}>
+          <Image
+            source={require('../Assets/Images/backward.png')}
+            style={styles.icons}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.8} style={styles.playAndPauseButton}>
+          <Image
+            source={require('../Assets/Images/play.png')}
+            style={styles.playAndPauseIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.8}>
+          <Image
+            source={require('../Assets/Images/fast-forward.png')}
+            style={styles.icons}
+          />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 const styles = StyleSheet.create({
+  slider: {
+    width: '100%',
+    height: 20,
+  },
   container: {
     flexDirection: 'row',
     height: '13%',
     justifyContent: 'center',
-    borderTopColor: 'orange',
-    borderTopWidth: 2,
     alignItems: 'center',
   },
   icons: {
